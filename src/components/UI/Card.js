8014c484e530e5e5993e7ee44e0524dfacb8c48e
pa-tiq <patrickdsilva99@gmail.com>
2022-09-29 15:51:08 -0400

    card WIP

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,9 +2,23 @@ import React from "react";
 import classes from "./Card.module.css";
 
 const Card = (props) => {
+  const cartItems = (
+    <ul>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
   return (
-    <div 
-      className={`${classes.card}`}>{props.children}
+    <div className={classes.card}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 };
