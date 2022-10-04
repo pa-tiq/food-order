c3abfabb33d079561ea3ad3e3d121df5abe23b04
Patrick Dantas <patrickdsilva99@gmail.com>
2022-10-03 21:21:05 -0400

    cart WIP

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./Cart.module.css";
 import Modal from "./Modal";
+import CartContext from "../../store/cart-context";
+
+const calculateTotalPrice = (items) => {
+  return items.reduce((currentNumber, item) => {
+    return currentNumber + item.price;
+  }, 0);
+};
 
 const Cart = (props) => {
+
+  const cartContext = useContext(CartContext);
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+      {cartContext.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
@@ -15,8 +25,8 @@ const Cart = (props) => {
     <Modal onHideCart={props.onHideCart}>
       {cartItems}
       <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>69.69</span>
+        <span>Total Price</span>
+        <span>{`$${calculateTotalPrice(cartContext.items)}`}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onHideCart}>
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,12 @@
-import React, { useContext} from "react";
+import React, { useContext } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
-const calculateNumberOfCartItems = (items) =>{
-  return (
-    items.reduce((currentNumber, item) => {
-      return currentNumber + item.amount;
-    }, 0)
-  ) 
+const calculateNumberOfCartItems = (items) => {
+  return items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
 };
 
 const HeaderCartButton = (props) => {
